36248737009a89afdcffd427825af6443e734b7e
Antonio Barber <antonio.barber@wsu.edu>
2021-03-23 02:36:11 -0700

    Created Main Menu

---
+++ BulletHell/BulletHell/States/GameState.cs
@@ -0,0 +1,144 @@
+﻿using BulletHell.Game_Utilities;
+using BulletHell.Sprites;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using global::BulletHell.Utilities;
+using Microsoft.Xna.Framework.Input;
+
+namespace BulletHell.States
+{
+    public class GameState : State
+    {
+        private SpriteBatch spriteBatch;
+        private List<Sprite> sprites;
+        private List<Wave> waves;
+        private double timeUntilNextWave = 0;
+        private State _currentState;
+
+        private State _nextState;
+
+        public GameState(BulletHell game, GraphicsDevice graphicsDevice, ContentManager content)
+        : base(game, graphicsDevice, content)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            this.spriteBatch.Begin();
+            foreach (var sprite in this.sprites)
+            {
+                sprite.Draw(this.spriteBatch);
+
+                if (sprite is Sprites.Entities.Player)
+                {
+                    Sprites.Entities.Player player = (Sprites.Entities.Player)sprite;
+                    if (player.slowMode)
+                    {
+                        this.DrawBoxAroundSprite(player);
+                        player.slowMode = false;
+                    }
+                }
+            }
+            this.spriteBatch.End();
+
+        }
+
+        public override void LoadContent()
+        {
+            this.spriteBatch = new SpriteBatch(_game.GraphicsDevice);
+
+            this.sprites = new List<Sprite>();
+
+            this.CreatePlayer();
+
+            this.CreateWaves();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            this.CheckAndDeployWave(gameTime);
+
+            foreach (var sprite in this.sprites.ToArray())
+            {
+                sprite.Update(gameTime, this.sprites);
+            }
+
+            this.PostUpdate();
+        }
+
+        private void CreatePlayer()
+        {
+            this.sprites.Add(GameLoader.LoadPlayer());
+        }
+
+        private void CreateWaves()
+        {
+            this.waves = GameLoader.LoadWaves();
+        }
+
+        private void CheckAndDeployWave(GameTime gameTime)
+        {
+            this.timeUntilNextWave -= gameTime.ElapsedGameTime.TotalSeconds;
+            if (this.timeUntilNextWave <= 0 && this.waves.Count > 0)
+            {
+                this.timeUntilNextWave = this.waves[0].waveDuration;
+                this.waves[0].CreateWave(this.sprites);
+                this.waves.RemoveRange(0, 1);
+            }
+        }
+
+        private void DrawBoxAroundSprite(Sprite sprite)
+        {
+            Texture2D hitboxTexture = new Texture2D(_game.GraphicsDevice, sprite.Rectangle.Width, sprite.Rectangle.Height);
+            Color[] data = new Color[sprite.Rectangle.Width * sprite.Rectangle.Height];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (i < sprite.Rectangle.Width)
+                {
+                    data[i] = Color.White;
+                }
+                else if (i % sprite.Rectangle.Width == 0)
+                {
+                    data[i] = Color.White;
+                }
+                else if (i % sprite.Rectangle.Width == sprite.Rectangle.Width - 1)
+                {
+                    data[i] = Color.White;
+                }
+                else if (i > (sprite.Rectangle.Width * sprite.Rectangle.Height) - sprite.Rectangle.Width)
+                {
+                    data[i] = Color.White;
+                }
+            }
+
+            hitboxTexture.SetData(data);
+
+            this.spriteBatch.Draw(hitboxTexture, new Vector2(sprite.Movement.Position.X - (hitboxTexture.Width / 2), sprite.Movement.Position.Y - (hitboxTexture.Height / 2)), Color.White);
+        }
+
+        private void PostUpdate()
+        {
+            for (int i = this.sprites.Count - 1; i >= 0; i--)
+            {
+                if (this.sprites[i].IsRemoved)
+                {
+                    this.sprites.RemoveAt(i);
+                }
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+        }
+    }
+}
---
+++ BulletHell/BulletHell/States/MenuState.cs
@@ -0,0 +1,128 @@
+﻿using BulletHell.Controls;
+using BulletHell.States.Emitters;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulletHell.States
+{
+    public class MenuState : State
+    {
+        private List<Component> _components;
+        private SnowEmitter _snowEmitter;
+        private SpriteBatch spriteBatch;
+
+
+        public object GraphicsDevice { get; private set; }
+
+        public MenuState(BulletHell game, GraphicsDevice graphicsDevice, ContentManager content)
+          : base(game, graphicsDevice, content)
+        {
+            var buttonTexture = _content.Load<Texture2D>("Controls/Button");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+
+            var newGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 200),
+                Text = "New Game",
+            };
+
+            newGameButton.Click += NewGameButton_Click;
+
+            var difficultyButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 250),
+                Text = "Select Difficulty",
+            };
+
+            difficultyButton.Click += DifficultyButton_Click;
+
+
+            var optionsButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 300),
+                Text = "Options",
+            };
+
+            optionsButton.Click += OptionsButton_Click;
+
+            var quitGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(300, 350),
+                Text = "Quit",
+            };
+
+            quitGameButton.Click += QuitGameButton_Click;
+
+
+            _components = new List<Component>()
+      {
+        newGameButton,
+        difficultyButton,
+        optionsButton,
+        quitGameButton,
+      };
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            _game.GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            foreach (var component in _components)
+                component.Draw(gameTime, spriteBatch);
+
+            _snowEmitter.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+        }
+
+        private void DifficultyButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Select Difficulty");
+        }
+
+        private void OptionsButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Options");
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            _game.ChangeState(new GameState(_game, _graphicsDevice, _content));
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            // remove sprites if they're not needed
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+                component.Update(gameTime);
+
+            _snowEmitter.Update(gameTime);
+        }
+
+        private void QuitGameButton_Click(object sender, EventArgs e)
+        {
+            _game.Exit();
+        }
+
+        public override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(_game.GraphicsDevice);
+
+            _snowEmitter = new SnowEmitter(new Emitters.SpriteLike(_content.Load<Texture2D>("Particles/Snow")));
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+        }
+    }
+}
